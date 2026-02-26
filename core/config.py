3e5e2fa0514a514c3d0d42f241143239eaac827d
Devarshi2285 <134229662+Devarshi2285@users.noreply.github.com>
2026-02-26 10:45:19 +0530

    Config changes -DB

--- core/config.py
+++ core/config.py
@@ -4,6 +4,10 @@ class Settings(BaseSettings):
     FRONTEND_URL: str
     ENV: str = "development"
     DATABASE_URL: str
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int
 
     class Config:
         env_file = ".env"
